b69e45d1e2108817086c225590d218af9859a5ce
Will Pearse <will.pearse@gmail.com>
2012-03-30 12:14:28 +0100

    Added simulations and report for submission to Heidelberg; data not included in release due to space and legality of releasing UKBMS data

--- main.cpp
+++ main.cpp
@@ -14,8 +14,35 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "data.h"
 using namespace std;
+
+static void print_iteration(vector<Data> results, int n_communities, int n_years, int total_individuals, int total_additions, boost::numeric::ublas::matrix<double> transition_matrix, vector<string> species_names)
+{
+    //Setup
+    stringstream ss;
+    ss << "results_" << n_communities << "-" << species_names.size() << "-" << n_years << "-" << total_individuals << "-" << total_additions << ".csv";
+    string filename(ss.str());
+    ofstream file;
+    file.open(filename.c_str());
+    
+    //Header
+    file << "iter,row,column,estimated,real" << endl;
+    //Loop through
+    for(int i=0; i<results.size(); ++i)
+    {
+        for(int j=0; j<results[i].transition_matrices[0].size1(); ++j)
+        {
+            for(int k=0; k<results[i].transition_matrices[0].size2(); ++k)
+            {
+                file << i << "," << j << "," << k << "," << results[i].transition_matrices[0](j,k) << "," << transition_matrix(j,k) << endl;
+            }
+        }
+    }
+}
+
 
 int main (int argc, const char * argv[])
 {
@@ -26,8 +53,81 @@ int main (int argc, const char * argv[])
     if(argc == 1)
     {
         cout << "To run with real data, enter two arguments: the community file, then the number of overall iterations" << endl;
-        cout << "To simulate, enter six arguments: the number of communities, number of years in each, number of species, number of starting individuals, the number of additions per year, a stable transition rate, and a random seed." << endl;
-        cout << "*Anything* else will either do nothing, or cause a confusing-looking error." << endl;
+        cout << "To run the simulations I present in the paper (the PDF), enter one argument: a random seed" << endl;
+        cout << "To do some simulations, enter six arguments: the number of communities, number of years in each, number of species, number of starting individuals, the number of additions per year, a stable transition rate, and a random seed." << endl;
+        cout << endl << "*Anything* else will either do nothing, or cause a confusing-looking error." << endl;
+    }
+    
+    if(argc == 2)
+    {
+        //////////////
+        //Setup
+        //////////////
+        int nComm[] = {5, 10, 20, 50};
+        int nYears[] = {5, 15, 30};
+        int nIndiv[] = {100, 200};
+        int nAdds[] = {5, 20};
+        double nFreqs[] = {0.2, 0.5, 0.8};
+        int nIter = 30;
+        int rnd_seed = atoi(argv[1]);
+        boost::mt19937 rnd_generator(rnd_seed);
+        vector<Data> iter_results;
+        
+        //////////////
+        //Tests
+        //////////////
+        //Create species' names
+        vector<string> sp_names(5);
+        char letter = 'a';
+        for(int i=0; i<5; ++i)
+            sp_names[i] = letter++;
+        
+        for(int com=0; com<4; ++com)
+        {
+            //Create community names
+            vector<string> community_names(nComm[com]);
+            char LETTER = 'A';
+            for(int i=0; i<nComm[com]; ++i)
+                community_names[i] = LETTER++;
+            
+            for(int yr=0; yr<3; ++yr)
+                for(int ind=0; ind<2; ++ind)
+                    for(int add=0; add<2; ++add)
+                        for(int frq=0; frq<3; ++frq)
+                        {
+                            //Create transition matrix
+                            double turnover_freq = (1.0 - nFreqs[frq]) / sp_names.size();
+                            double add_freq = 1.0 / sp_names.size();
+                            boost::numeric::ublas::matrix<double> transition_matrix(sp_names.size(), sp_names.size()+2);
+                            for(int i=0; i<transition_matrix.size1(); ++i)
+                            {
+                                int j=0;
+                                for(j=0; j<(transition_matrix.size2()-1); ++j)
+                                    if(i==j)
+                                        transition_matrix(i,j) = nFreqs[frq];
+                                    else
+                                        transition_matrix(i,j) = turnover_freq;
+                                transition_matrix(i,j) = add_freq;
+                            }
+                            
+                            for(int iter=0; iter<nIter; ++iter)
+                            {
+                                //Random number
+                                int local_seed = rnd_generator();
+                                
+                                //Create data...
+                                Data data(nComm[com], nYears[yr], nIndiv[ind], nAdds[add], sp_names, transition_matrix, community_names, local_seed);
+                                
+                                //Optimise
+                                data.optimise(0,0);
+                                
+                                //Store
+                                iter_results.push_back(data);
+                            }
+                            print_iteration(iter_results, nComm[com], nYears[yr], nIndiv[ind], nAdds[add], transition_matrix, sp_names);
+                            iter_results.clear();
+                        }
+            }
     }
     
     if(argc == 3)
